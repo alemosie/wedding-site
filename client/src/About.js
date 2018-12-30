c6a894372a31fb018b74968df109ead18e64b798
alemosie <alexandra.siega@gmail.com>
2018-12-30 11:12:56 -0500

    Create timeline data, table

--- client/src/About.js
+++ client/src/About.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from 'react-bootstrap';
+import timelineData from './data/dates.json';
 
 class About extends Component {
+
+  createTable = (data) => {
+    let tableRows = data.map(row => {
+      let tableRow = row.map((element, index) => {
+        return <td key={row[0] + row[1] + index}>{element}</td>
+      })
+      return <tr key={row[0] + row[1]}>{tableRow}</tr>
+    })
+    
+    return <table id="timeline"><tbody>{tableRows}</tbody></table>
+  }
+
   render() {
     return (
       <Grid>
@@ -14,6 +27,7 @@ class About extends Component {
           </Col>
           <Col xs={8} md={4}>
             <h2>Timeline</h2>
+            {this.createTable(timelineData)}
           </Col>
         </Row>
       </Grid>
