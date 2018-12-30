f7f5e97f4aa9db05fabf64b942e09502db64198f
alemosie <alexandra.siega@gmail.com>
2018-12-30 16:01:41 -0500

    Add photo slideshow to About page

--- client/src/About.js
+++ client/src/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from 'react-bootstrap';
 import timelineData from './data/dates.json';
+import Slideshow from './Slideshow'
 import './stylesheets/About.css';
 
 class About extends Component {
@@ -30,30 +31,37 @@ class About extends Component {
       <Grid>
         <Row className="show-grid">
           <Col xs={10} md={8}>
-            <h1>How we met</h1>
-            <h4>BLUF: Food and Dostoyevsky on a train makes for a successful pick-up scheme.</h4>
-            <p>
-              We apparently met at an internship orientation. 
-              Neither of us remembers this (and only discovered this searching through G-mail history), 
-              so how we <span>really</span> met is on a train platform heading back from work to Washington, D.C., where we were both living at the time.
-            </p>
-            <p>
-              Mike was part of the elite "Club MARC", a band of coworkers that brought wine and cheese every Friday to enjoy on the train back home to D.C.
-              Alex was not part of this club, but had heard rumors and wanted in. (Obviously.) 
-              She spotted the group on the train platform, approached them, and became part of the fold. 
-              In addition to scoring some delicious train cheese, she got to sit next to Mike on the way back home. 
-            </p>
-            <p>
-              On that train ride, we somehow landed on the topic of Russian literature. 
-              Alex was a Russian Studies major, and had recently read Crime and Punishment by Fyodor Dostoyevsky.
-              One of Mike's favorite novels was The Brothers Karamazov, also by Dostoyevsky. 
-              Neither had read the other, and Mike proposed the idea of swapping books and meeting up at a coffeeshop to discuss them when we were finished.
-              Mike intended this to be a date, and read Crime and Punishment in a week.
-              Alex didn't realize that it was a date, and took 3 months to read The Brothers Karamazov.
-            </p>
-            <p>
-              At least we got there in the end.
-            </p>
+            <div id="how-we-met">
+              <h1>How we met</h1>
+              <h4>BLUF: Food and Dostoevsky on a train make for a bizarre but effective start of something grand.</h4>
+              <p>
+                They apparently met at an internship orientation. 
+                Neither of them remembers this (and only discovered it via Gmail history), 
+                so where they <span className="emphasis">really</span> met is on a train after work one day.
+              </p>
+              <p>
+                Mike was part of the elite "Club <a href="https://en.wikipedia.org/wiki/MARC_Train">MARC</a>", 
+                a band of coworkers that brought wine and cheese to enjoy on the train back home to Washington, D.C. every Friday.
+                Alex was not part of this club, but had heard rumors and wanted in. (Obviously.) 
+                She approached the group on the train platform and asked to join.
+                In addition to scoring some delicious train cheese, she got to sit next to Mike on the way home. 
+              </p>
+              <p>
+                On that train ride, they somehow landed on the topic of Russian literature. 
+                Alex was a Russian Studies major, and had recently 
+                read <a href="https://en.wikipedia.org/wiki/Crime_and_Punishment">Crime and Punishment</a> by Fyodor Dostoevsky.
+                One of Mike's favorite novels was <a href="https://en.wikipedia.org/wiki/The_Brothers_Karamazov">The Brothers Karamazov</a>, 
+                also by Dostoevsky. Neither had read the other, and Mike proposed the idea of swapping books and meeting up at a coffeeshop 
+                to discuss them when they were finished.
+                Mike intended for this to be a date, and read Crime and Punishment in a week in anticipation.
+                Alex didn't realize this, and instead took 3 months to read The Brothers Karamazov.
+                But it worked out in the end!
+              </p>
+            </div>
+            <div>
+              <h2>Through the years...</h2>
+              <Slideshow />
+            </div>
           </Col>
           <Col xs={8} md={4} className="timeline-container">
             <h2 className="center">Timeline</h2>
