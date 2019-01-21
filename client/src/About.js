ef92eff98ccdadb79a5b8a00eee66a250e2a4f86
alemosie <alexandra.siega@gmail.com>
2019-01-21 17:45:54 -0500

    Create Wedding page

--- client/src/About.js
+++ client/src/About.js
@@ -19,18 +19,18 @@ class About extends Component {
     let tableRows = data.map(row => {
       let tableRow = row.map((element, index) => {
         let cell = this.createCell(element, index, row[3])
-        return React.cloneElement(cell, {key:  row[0] + row[1] + index})
+        return React.cloneElement(cell, { key: row[0] + row[1] + index })
       })
       return <tr key={row[0] + row[1]}>{tableRow}</tr>
     })
-    
+
     return <table id="timeline"><tbody>{tableRows}</tbody></table>
   }
 
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <div className="content">
           <Grid>
             <Row className="show-grid">
@@ -39,23 +39,23 @@ class About extends Component {
                   <h1>How we met</h1>
                   <h4>BLUF: Food and Dostoevsky on a train make for a bizarre but effective start of something grand.</h4>
                   <p>
-                    They apparently met at an internship orientation. 
-                    Neither of them remembers this (and only discovered it via Gmail history), 
+                    They apparently met at an internship orientation.
+                    Neither of them remembers this (and only discovered it via Gmail history),
                     so where they <span className="emphasis">really</span> met is on a train after work one day.
                   </p>
                   <p>
-                    Mike was part of the elite "Club <a href="https://en.wikipedia.org/wiki/MARC_Train">MARC</a>", 
+                    Mike was part of the elite "Club <a href="https://en.wikipedia.org/wiki/MARC_Train">MARC</a>",
                     a band of coworkers that brought wine and cheese to enjoy on the train back home to Washington, D.C. every Friday.
-                    Alex was not part of this club, but had heard rumors and wanted in. (Obviously.) 
+                    Alex was not part of this club, but had heard rumors and wanted in. (Obviously.)
                     She approached the group on the train platform and asked to join.
-                    In addition to scoring some delicious train cheese, she got to sit next to Mike on the way home. 
+                    In addition to scoring some delicious train cheese, she got to sit next to Mike on the way home.
                   </p>
                   <p>
-                    On that train ride, they somehow landed on the topic of Russian literature. 
-                    Alex was a Russian Studies major, and had recently 
+                    On that train ride, they somehow landed on the topic of Russian literature.
+                    Alex was a Russian Studies major, and had recently
                     read <a href="https://en.wikipedia.org/wiki/Crime_and_Punishment">Crime and Punishment</a> by Fyodor Dostoevsky.
-                    One of Mike's favorite novels was <a href="https://en.wikipedia.org/wiki/The_Brothers_Karamazov">The Brothers Karamazov</a>, 
-                    also by Dostoevsky. Neither had read the other, and Mike proposed the idea of swapping books and meeting up at a coffeeshop 
+                    One of Mike's favorite novels was <a href="https://en.wikipedia.org/wiki/The_Brothers_Karamazov">The Brothers Karamazov</a>,
+                    also by Dostoevsky. Neither had read the other, and Mike proposed the idea of swapping books and meeting up at a coffeeshop
                     to discuss them when they were finished.
                     Mike intended for this to be a date, and read Crime and Punishment in a week in anticipation.
                     Alex didn't realize this, and instead took 3 months to read The Brothers Karamazov.
@@ -78,5 +78,5 @@ class About extends Component {
     );
   }
 }
- 
+
 export default About;
--- client/src/Wedding.js
+++ client/src/Wedding.js
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
 import Header from './Header'
+import { Image } from 'react-bootstrap'
+import headerImage from './images/std-square.jpg'
+
+import './stylesheets/Wedding.css';
 
 class Wedding extends Component {
+  googleMapsLink = "https://www.google.com/maps/place/Lyman+Estate/@42.383114,-71.228233,15z/data=!4m5!3m4!1s0x0:0xf5ae171d528e9814!8m2!3d42.3831144!4d-71.228233?hl=en-US"
+  googleCalLink = "https://calendar.google.com/calendar/r/eventedit?dates=20190121/20190122&text=Mike+and+Alex%27s+Wedding&location=185+Lyman+St,+Waltham,+MA+02452&details&sf=true"
+
   render() {
     return (
       <div>
-        <Header/>
-        <div className="content">
-          <h2>The Wedding</h2>
+        <Header />
+        <div className="content wedding-content-base-container">
+          {/* <div className="triangle">
+            <span className="top right">◤</span>
+            <span className="top left">◥</span>
+          </div> */}
+          <div className="wedding-content-container">
+            <div className="wedding-content">
+              <div className="wedding-content-image">
+                <Image src={headerImage} circle />
+              </div>
+              <div className="wedding-content-text">
+                <h1>Mike & Alex's Wedding</h1>
+                <h3>Saturday, <span className="date">September 21, 2019</span></h3>
+                <h3>Lyman Estate</h3>
+                <div className="address-container">
+                  <a href={this.googleCalLink}>
+                    <p>185 Lyman St</p>
+                    <p>Waltham, MA 02452</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p className="emphasis">More details to follow</p>
+
         </div>
       </div>
     );
   }
 }
- 
+
 export default Wedding;
